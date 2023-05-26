f7cea880381cf2f0fb9e2b9490af3c1269c4867d
GG ALLIN <120009426+obdrystish@users.noreply.github.com>
2023-05-26 13:31:06 +0300

    Delete listing directory

--- listing/ExplicitCast.java
+++
@@ -1,7 +0,0 @@
-public class ExplicitCast {
-    public static void main(String[] args){
-        long l = 10;
-        double d = 1;
-        l = (long) d;
-    } // main(String[]
-} // ExplicitCast class
--- listing/GradualCastDemo.java
+++
@@ -1,9 +0,0 @@
-public class GradualCastDemo {
-    public static void main(String[] args)
-byte x, z;
-int y;
-x=5
-y = x * x;
-z = {byte} (x*x);
-} // main (String[]) method
-} // GradualCastDemo class
