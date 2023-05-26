57ac711400887d2bc6450adeff097190d2cde1da
GG ALLIN <120009426+obdrystish@users.noreply.github.com>
2023-05-26 13:38:46 +0300

    Delete listing directory

--- listing/listing2/ExplicitCast.java
+++
@@ -1,9 +0,0 @@
-package ru.vshp.listing2;
-
-public class ExplicitCast {
-    public static void main(String[] args) {
-        long l = 10;
-        double d = l;
-        l = (long) d;
-    } // main(String[] ) method
-} // ExplicitCast class
--- listing/listing6/ShortcutOutDemo.java
+++
@@ -1,8 +0,0 @@
-package ru.vshp.listing6;
-
-public class ShortcutOutDemo {
-    private static void main(String[] args) {
-        out.println("Пример...");
-        out.println("...краткой записи команды вывода на консоль...");
-    }
-}
